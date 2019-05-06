7eb4c7b190a430194ff45b10df5dea680931c5fd
Ray Kang <adamck6302438@gmail.com>
2019-05-06 15:10:15 -0400

    first commit

---
+++ MathGame/Player.h
@@ -0,0 +1,17 @@
+//
+//  Player.h
+//  MathGame
+//
+//  Created by Frances ZiyiFan on 5/6/19.
+//  Copyright © 2019 Ray Kang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Player : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ MathGame/Player.m
@@ -0,0 +1,13 @@
+//
+//  Player.m
+//  MathGame
+//
+//  Created by Frances ZiyiFan on 5/6/19.
+//  Copyright © 2019 Ray Kang. All rights reserved.
+//
+
+#import "Player.h"
+
+@implementation Player
+
+@end
