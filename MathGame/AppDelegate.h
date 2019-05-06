6d8195a13450646f95411dbdad3dfa2eb948fac6
Ray Kang <adamck6302438@gmail.com>
2019-05-06 11:24:00 -0400

    Initial Commit

---
+++ MathGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MathGame
+//
+//  Created by Frances ZiyiFan on 5/6/19.
+//  Copyright © 2019 Ray Kang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
